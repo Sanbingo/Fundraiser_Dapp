c4c38b8d0cde8c1ea798840b528bd0ae07fef376
san <linrisan@qq.com>
2022-08-28 15:18:57 +0800

    feat: 测试环境部署

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,10 @@
+require('dotenv').config();
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+const infuraKey = process.env.INFURA_API_KEY || ""
+
+console.log('Keys', privateKeys, infuraKey)
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,7 +15,23 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: 5777
-    }
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider([privateKeys], `https://mainnet.infura.io/v3/` + infuraKey),
+      network_id: 1,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },    
+    ropsten: {
+      provider: () => new HDWalletProvider([privateKeys], `https://ropsten.infura.io/v3/` + infuraKey),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   compilers: {
     solc: {
